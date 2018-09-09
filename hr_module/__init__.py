eef3a19216e83e06fc7294ce5d9f239a60bfe986
sjain <jainshaily29@gmail.com>
2018-09-09 09:37:34 +0100

    Project Setup

---
+++ hr_module/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""UFIX Project"""
+
+from hr_module import metadata
+
+
+__version__ = metadata.version
+__author__ = metadata.authors[0]
+__license__ = metadata.license
+__copyright__ = metadata.copyright
---
+++ hr_module/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'hr_module'
+project = "UFIX"
+project_no_spaces = project.replace(' ', '')
+version = '0.1'
+description = 'UFIX Project'
+authors = ['Shaily', 'Jain']
+authors_string = ', '.join(authors)
+emails = ['shaily.jain@uwic.ac.uk']
+license = 'MIT'
+copyright = '2018 ' + authors_string
+url = 'http://www.example.com/'
